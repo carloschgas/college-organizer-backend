7bf41fc746908c7068f52af1cadf781506273e68
carloschgas <139183141+carloschgas@users.noreply.github.com>
2025-11-09 19:09:05 -0300

    feat: first commit

---
+++ src/main/java/com/carloschgas/college_organizer/CollegeOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.carloschgas.college_organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carloschgas/college_organizer/CollegeOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloschgas.college_organizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
